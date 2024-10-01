d52b911fe9dcf18e64cd5355357dd83d0614e623
Felix Doan <142334591+FelixDoan2005@users.noreply.github.com>
2024-10-01 23:26:28 +0000

    Sync LeetCode submission Runtime - 17 ms (59.62%), Memory - 11.5 MB (100.00%)

---
+++ leetcode/0007-reverse-integer/solution.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def reverse(self, x):
+        y = 0
+        if x < 0:
+            y = int(str(x)[1:][::-1]) * -1
+        else:
+            y = int(str(x)[::-1])
+        
+        if y > 2 ** 31 -1 or y < -2 ** 31:
+            return 0
+
+        return y
